ca07c888b12b25814d7d6894c81add5cd0adc330
dehwyy <91pony@mail.ru>
2023-02-17 00:17:24 +0300

    form:before

--- src/Widgets/Items/Post.react.tsx
+++ src/Widgets/Items/Post.react.tsx
@@ -7,30 +7,33 @@ interface IProps {
 
 const Post:FC<IProps> = ({post}) => {
     return (
-        <div className="bg-[#444444] text-white p-7 cut-right-bottom-big flex flex-col gap-y-5">
-            <div className="flex items-center gap-x-3">
-                <div className="w-[30px] h-[30px] rounded-full">
-                    <img className="w-full h-full object-cover rounded-full" src={post.authorImage} alt="image"/>
+        <a href={`post/${post.id}`}>
+            <div className="bg-[#444444] text-white p-7 cut-right-bottom-big flex flex-col gap-y-5">
+                <div className="flex items-center gap-x-3">
+                    <div className="w-[30px] h-[30px] rounded-full">
+                        <img className="w-full h-full object-cover rounded-full" src={post.authorImage} alt="image"/>
+                    </div>
+                    <div className="font-custom-neuro text-custom-light-blue">{post.author}</div>
+                    <div className="font-custom-zekton text-[10px] pt-1 text-custom-gray">{post.date}</div>
                 </div>
-                <div className="font-custom-neuro text-custom-light-blue">{post.author}</div>
-                <div className="font-custom-zekton text-[10px] pt-1 text-custom-gray">{post.date}</div>
+                <div className="text-[23px] font-custom-zekton text-custom-light-blue">
+                    - {post.title}
+                </div>
+                <div className="flex gap-x-3 text-[12px] font-custom-zekton text-custom-gray">
+                    <div>-- {post.theme}</div>
+                    {post.tags.map((tag, index) => (
+                        <div key={index}>-- {tag}</div>
+                    ) )}
+                </div>
+                <div className="">
+                    <img className="max-h-[550px] h-full w-full object-cover" src={post.previewImage} alt="image" />
+                </div>
+                <div className="">
+                    {post.previewText}
+                </div>
             </div>
-            <div className="text-[23px] font-custom-zekton text-custom-light-blue">
-                - {post.title}
-            </div>
-            <div className="flex gap-x-3 text-[12px] font-custom-zekton text-custom-gray">
-                <div>-- {post.theme}</div>
-                {post.tags.map((tag, index) => (
-                    <div key={index}>-- {tag}</div>
-                ) )}
-            </div>
-            <div className="">
-                <img className="max-h-[550px] h-full w-full object-cover" src={post.previewImage} alt="image" />
-            </div>
-            <div className="">
-                {post.previewText}
-            </div>
-        </div>
+        </a>
+
     );
 };
 
--- src/shared/utils/setNavbarColor.ts
+++ src/shared/utils/setNavbarColor.ts
@@ -1,21 +1,15 @@
 enum UrlCases {
     blogs = "/blogs",
+    post = "/post",
     subscriptions = "/subs",
     profile = "/user",
     create = "/create",
 }
 export default function setNavbarColor(url: string): string {
-    switch (url) {
-        case UrlCases.blogs:
-            return "bg-custom-blue"
-        case UrlCases.subscriptions:
-            return "bg-custom-midnight"
-        case UrlCases.profile:
-            return "bg-custom-dark-purple"
-        case UrlCases.create:
-            return "bg-custom-purple"
-        default:
-            return "bg-custom-darkred"
-
-    }
+    if (url.match(UrlCases.blogs)) return "bg-custom-blue"
+    if (url.match(UrlCases.post)) return "bg-custom-red"
+    if (url.match(UrlCases.subscriptions)) return "bg-custom-midnight"
+    if (url.match(UrlCases.profile)) return "bg-custom-dark-purple"
+    if (url.match(UrlCases.create)) return "bg-custom-purple"
+    return "bg-custom-darkred"
 }
